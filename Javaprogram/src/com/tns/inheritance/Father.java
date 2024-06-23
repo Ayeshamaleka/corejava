dc5e6ac1a59d3abff6769c801f9d76ae1c96436f
wasim <wasim@WSH>
2024-06-23 15:05:38 +0530

    commit message

---
+++ Javaprogram/src/com/tns/inheritance/Father.java
@@ -0,0 +1,16 @@
+package com.tns.inheritance;
+
+public class Father {
+	int money=9;
+	String car="BMW";
+	
+	void drinking() {
+		System.out.println("tae");
+		
+	}
+	
+	void learn()  {
+		System.out.println("newspaper");
+	}
+
+}
---
+++ Javaprogram/src/com/tns/inheritance/Singleinheritance.java
@@ -0,0 +1,16 @@
+package com.tns.inheritance;
+
+public class Singleinheritance {
+	
+	public static void main(String[] args) {
+		Student s1=new Student("ayesha",357864389,"hyderabad",910003284);
+		
+		Student s2=new Student(1,"shadan");
+		
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		
+	}
+
+}
