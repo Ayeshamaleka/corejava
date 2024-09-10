bad0d835667a05748e1b1249854942bd068e3a04
wasim <wasim@WSH>
2024-09-10 19:00:55 +0530

    commit message

--- Javaprogram/src/module-info.java
+++ Javaprogram/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module Javaprogram {
+	requires java.sql;
 }
---
+++ corejavaprogram/src/com/tns/jdbcdemo/JdbcDemo.java
@@ -0,0 +1,45 @@
+package com.tns.jdbcdemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcDemo {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		//load the driver
+		Class.forName("org.postgres.Driver");
+		
+		//get Connection
+		Connection con=DriverManager.getConnection("jdbc:postgresql:"
+				+"//localhost:5432/shadan","postgres","ayesha01");
+		System.out.println("connection established");
+
+		//create statement 
+		Statement stmt=con.createStatement();
+		
+		//write the query
+				String sql="selet * FROM stud";
+				
+		//read the data 
+		ResultSet rs=stmt.executeQuery(sql);
+				
+		//traverse through the table 
+		while(rs.next()) {
+			System.out.println(rs.getInt(1)+ "/t" +rs.getString(2)
+					                            +"/t"+rs.getString(3));
+				}
+
+    	//close statement
+		stmt.close();
+				
+	    //close connection
+		con.close();
+	
+	}
+
+	
+}
